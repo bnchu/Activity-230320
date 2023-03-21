0d131e3f5bf8c9ca9770404a92f24906839f6750
bnchu <lbo0947@dlsud.edu.ph>
2023-03-21 15:02:45 +0800

    Add activity code

---
+++ indexActivity.js
@@ -0,0 +1,21 @@
+ console.log('Hello, Welcome to De La Salle University!');
+
+let firstName = 'Benedict Chantelle';
+let lastName = 'Ledesma';
+let age = 21;
+let hobbies = ['Listening to Music', 'Watch Movie/Tv  Series', 'Go outdoors', 'Do Households', 'Read Digital Books'];
+let homeAddress = {
+	houseNumber: '2218',
+	street: 'Villar Avenue',
+	subdivision: 'Camella Springville',
+	barangay: 'Molino IV',
+	city: 'Bacoor',
+	province: 'Cavite',
+	zipCode: 4102
+};
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
